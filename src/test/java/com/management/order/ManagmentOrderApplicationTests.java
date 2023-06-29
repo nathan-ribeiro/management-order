43e9d43cfd67c14f1dfe68fdac1d572dbdd31e7a
nathan-ribeiro <nathanribeiro778@gmail.com>
2023-06-29 14:41:37 -0300

    Starting project

---
+++ src/test/java/com/management/order/ManagmentOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagmentOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
